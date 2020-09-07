a223d3ecaf0fca882f60bad30d5470a7111eafd5
Brittney <brittney.postma@hotmail.com>
2020-09-07 13:25:48 -0400

    variants

--- src/Accordion.js
+++ src/Accordion.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 
+const variants = {
+  open: { opacity: 1, height: 'auto' },
+  closed: { opacity: 1, height: 0 }
+}
+
 const Accordion = ({ title, body }) => {
   const [isToggled, setIsToggled] = useState(false)
   return (
@@ -9,10 +14,11 @@ const Accordion = ({ title, body }) => {
         <h2 role="button" onClick={() => setIsToggled(prevState => !prevState)}>The Heading</h2>
         {isToggled && (
           <motion.div
+            variants={variants}
             style={{ overflow: "hidden" }}
-            initial={{ opacity: 0, height: 0 }}
-            animate={{ opacity: 1, height: 'auto' }}
-            exit={{ opacity: 0, height: 0 }}
+            initial="closed"
+            animate="open"
+            exit="closed"
           >
             <p>I'm baby brooklyn tbh sartorial selvage pok pok poutine keytar hot chicken. Taxidermy humblebrag vaporware neutra viral air plant messenger bag. Thundercats kogi cronut, offal bitters keffiyeh brooklyn pickled skateboard next level ethical stumptown squid hot chicken. Master cleanse mixtape coloring book, cray ramps sriracha celiac butcher man braid kombucha. Woke hot chicken pour-over synth pitchfork scenester flexitarian 3 wolf moon lyft 90's XOXO sriracha tumblr. Distillery palo santo bushwick stumptown, sriracha unicorn locavore authentic flannel hexagon prism typewriter whatever chartreuse meh.</p>
           </motion.div>
--- src/App.js
+++ src/App.js
@@ -25,8 +25,11 @@ function App() {
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      animate={{ opacity: [0, 1, 0, 1] }}
-      transition={{ duration: 5, times: [0, 0.2, 0.3, 1] }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    // initial={{ opacity: 0 }}
+    // animate={{ opacity: [0, 1, 0, 1] }}
+    // transition={{ duration: 5, times: [0, 0.2, 0.3, 1] }}
     >
       <Header>
         <Menu />
